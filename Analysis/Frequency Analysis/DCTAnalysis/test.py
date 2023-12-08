58d9bf59a1775dc4c5d4efa54c4284abd2486044
Harshita Poojary <dmpoojary6458@gmail.com>
2023-12-08 09:41:02 -0800

    Modified files

--- Analysis/Frequency Analysis/DCTAnalysis/test.py
+++ Analysis/Frequency Analysis/DCTAnalysis/test.py
@@ -3,6 +3,6 @@ import os
 def count_files(directory):
     return len([name for name in os.listdir(directory) if os.path.isfile(os.path.join(directory, name))])
 
-directory_path = r'C:\Users\dmpoo\OneDrive\Desktop\Yamini\realvsfake_merged_cropped\real_cropped'
+directory_path = r'C:\Users\dmpoo\OneDrive\Desktop\realvsfake_merged_cropped\real_cropped'
 file_count = count_files(directory_path)
 print(f"Number of files in the directory: {file_count}")
